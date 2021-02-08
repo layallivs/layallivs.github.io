9e8c58a36dde8638c7e51e594a46c9bb5c919b0a
Konrad Werys <konradwerys2@gmail.com>
2021-02-08 14:44:04 +0100

    fix About width and social media buttons

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,12 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
-  },
+        extend: {
+          backgroundImage: theme => ({
+           'layal-hero': "url('images/LayalLiverpool3.jpg')",
+          })
+        }
+      },
   variants: {
     extend: {},
   },
